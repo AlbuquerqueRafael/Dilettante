3ad39ee10584ca2e3c7cfc06d18af672aff32c59
unknown <rafael.sousa@ccc.ufcg.edu.br>
2018-06-08 19:19:32 -0300

    Adding more to the components

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -6,6 +6,10 @@ export default function userReducer(state = initialState.user, action) {
   switch(action.type) {
     case types.LOGIN_SUCCESS:
       return action.responseUser;
+    case types.LOGIN_REQUEST:
+      return action.responseUser;
+    case types.LOGIN_ERROR:
+      return action.responseUser;
     default: 
       return state;
   }
